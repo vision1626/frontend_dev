c64b4535a196e93e253f55450b3fcd886b7277c4
Ed Lee <jonexiu@gmail.com>
2015-12-24 22:30:03 +0800

    fix header style.

--- global_header/Gruntfile.js
+++ global_header/Gruntfile.js
@@ -62,12 +62,16 @@ module.exports = function (grunt) {
 
         autoprefixer: {
             dist: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= globalConfig.cssDistPath %>',
-                    src: '**/*.css',
-                    dest: '<%= globalConfig.cssDistPath %>'
-                }]
+                //files: [{
+                //    expand: true,
+                //    cwd: '<%= globalConfig.cssDistPath %>',
+                //    src: '**/*.css',
+                //    dest: '<%= globalConfig.cssDistPath %>'
+                //}]
+                files: {
+                    '<%= globalConfig.cssDistPath %>/<%= globalConfig.viewName %>.css':
+                        '<%= globalConfig.cssDistPath %>/<%= globalConfig.viewName %>.css'
+                }
             }
         },
 
--- global_header/source/coffee/global_header.coffee
+++ global_header/source/coffee/global_header.coffee
@@ -1,6 +1,21 @@
 init_global_header = ->
-  $search = $('.main-nav__search').find('input');
-  $search_icon_font = $('.main-nav__search').find('i');  
+  $search_wrap = $('.main-nav__search')
+  $search = $search_wrap.find('input')
+  $search_icon_font = $search_wrap.find('i')
+  $nav_right = $('.main-nav--right')
+  $nav_left = $('.main-nav')
+  $nav_right_w = $nav_right.width()
+  $nav_left_w = $nav_left.width()
+
+  set_search_w = ->
+    win_w = $(window).width()
+    $search_wrap.width win_w - $nav_right_w - $nav_left_w - 40 - 12
+
+  set_search_w()
+  $(window).on "resize", ->
+    set_search_w()
+
+
 
   # Event registers
   $search.on 'focus', ->
