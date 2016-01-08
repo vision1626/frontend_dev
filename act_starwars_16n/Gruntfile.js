14815004b8a5b5da91617b9bef5ffc3b345ac3ea
Kenny Li <kenny@fcmvision.com>
2016-01-08 10:37:21 +0800

    Starwars

---
+++ act_starwars_16n/Gruntfile.js
@@ -0,0 +1,131 @@
+'use strict';
+module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
+
+    var globalConfig = {
+        src: 'src',
+        dest: 'dev',
+        actName:'starwars',
+        cssSrcPath: 'source/less',
+        jsSrcPath: 'source/coffee',
+        jadeSrcPath: 'source/jade',
+        cssDistPath: '../../tpl/hi1626/css/act/<%= globalConfig.actName %>',
+        jsDebugPath: '../../tpl/hi1626/js/act/<%= globalConfig.actName %>/debug',
+        jsDistPath: '../../tpl/hi1626/js/act/<%= globalConfig.actName %>',
+        htmlDistPath: '../../tpl/hi1626/page/act/<%= globalConfig.actName %>',
+        cssName: '<%= globalConfig.actName %>',
+        jsName: '<%= globalConfig.actName %>',
+    };
+
+    grunt.initConfig({
+        globalConfig: globalConfig,
+        watch: {
+            less: {
+                files: ['<%= globalConfig.cssSrcPath %>/*.less'],
+                tasks: ['less', 'autoprefixer', 'cssmin']
+            },
+            js: {
+                files: ['<%= globalConfig.jsSrcPath %>/*.coffee'],
+                tasks: ['coffee', 'uglify']
+            },
+            jade:{
+                files: ['<%= globalConfig.jadeSrcPath %>/*.jade'],
+                tasks: ['jade4php']
+            }
+        },
+
+        cssmin: {
+            target: {
+                files: {
+                    '<%= globalConfig.cssDistPath %>/<%= globalConfig.cssName %>.min.css': [
+                        '<%= globalConfig.cssDistPath %>/<%= globalConfig.cssName %>.min.css'
+                    ]
+                }
+            }
+        },
+
+        less: {
+            dist: {
+                files: {
+                    '<%= globalConfig.cssDistPath %>/<%= globalConfig.cssName %>.min.css': [
+                        '<%= globalConfig.cssSrcPath %>/app.less'
+                    ]
+                },
+                options: {
+                    compress: false,
+                    sourceMap: true,
+                    sourceMapFilename: '<%= globalConfig.cssDistPath %>/<%= globalConfig.cssName %>.min.css.map',
+                    sourceMapURL: '<%= globalConfig.cssName %>.min.css.map'
+                }
+            }
+        },
+
+        autoprefixer: {
+            dist: {
+                files: {
+                    '<%= globalConfig.cssDistPath %>/<%= globalConfig.cssName %>.min.css': [
+                        '<%= globalConfig.cssDistPath %>/<%= globalConfig.cssName %>.min.css'
+                    ]
+                }
+            }
+        },
+
+        coffee: {
+            options: {
+                sourceMap: true,
+                bare: true
+            },
+            compile: {
+                files: {
+                    '<%= globalConfig.jsDebugPath %>/<%= globalConfig.jsName %>.js':[
+                        '<%= globalConfig.jsSrcPath %>/util.coffee',
+                        '<%= globalConfig.jsSrcPath %>/arttitude.coffee'
+                    ]
+                }
+            }
+        },
+
+        uglify: {
+            options: {
+                beautify: false,
+                compress: {
+                    drop_console: true
+                }
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= globalConfig.jsDebugPath %>',
+                    src: '**/*.js',
+                    dest: '<%= globalConfig.jsDistPath %>'
+                }]
+            }
+        },
+
+        jade4php: {
+            compile: {
+                options: {
+                    pretty: true
+                },
+                expand: true,
+                cwd: '<%= globalConfig.jadeSrcPath %>/',
+                src: ['*.jade'],
+                dest: '<%= globalConfig.htmlDistPath %>/',
+                ext: '.htm'
+            }
+        }
+    });
+
+    // Register tasks
+    grunt.registerTask('default', [
+        'less',
+        'autoprefixer',
+        'cssmin',
+        'coffee',
+        'uglify',
+        'jade4php'
+    ]);
+    grunt.registerTask('dev', [
+        'watch'
+    ]);
+};
