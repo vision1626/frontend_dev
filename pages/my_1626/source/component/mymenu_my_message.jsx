3d19403bb3a6620ee8e9364ee2d90fb289c95f5d
Kenny Li <kenny@fcmvision.com>
2016-02-29 15:11:48 +0800

    V2 加入菜单再拆分

--- pages/my_1626/source/component/mymenu_my_message.jsx
+++ pages/my_1626/source/component/mymenu_my_message.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import BaseComponent from '../script/BaseClass.jsx';
+import MenuItem from 'mymenu_item.jsx';
 class M_Message extends BaseComponent {
   constructor() {
     super();
@@ -19,6 +20,7 @@ class M_Message extends BaseComponent {
               <span className="m_number">{this.userStatistics.msg_like_count}</span>
             </a>
           </dd>
+          <MenuItem text="收到的喜欢收到的喜欢"/>
           <dd className="m_item">
             <a>
               收到的喜欢
