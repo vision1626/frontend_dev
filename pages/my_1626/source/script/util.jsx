0518cc52d82667c95619b0c57fe15e9610dd00f9
Kenny Li <kenny@fcmvision.com>
2016-03-04 16:24:09 +0800

    V2 个人中心加入一个公用方法

--- pages/my_1626/source/script/util.jsx
+++ pages/my_1626/source/script/util.jsx
@@ -8,3 +8,11 @@ export function formatDate(source){
   let td = new Date(source);
   return [[td.getFullYear,td.getMonth(),td.getDate].join('-'),[td.getHours(),td.getMinutes(),td.getSeconds()].join(':')].join(' ')
 }
+
+export function showError(title,state,message){
+  if (typeof console !== 'undefined') {
+    console.error(title,state,message);
+  } else {
+    alert([title,state,message].join(','));
+  }
+}
