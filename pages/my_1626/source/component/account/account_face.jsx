4d6a73249ec6c8b6df2c69b9d3c89178b0a950ad
Kenny Li <kenny@fcmvision.com>
2016-04-12 11:26:25 +0800

    V2 个人中心 头像上传

--- pages/my_1626/source/component/account/account_face.jsx
+++ pages/my_1626/source/component/account/account_face.jsx
@@ -8,6 +8,7 @@ class Entity extends BaseComponent {
     super();
     this.handlerSubmitClick = this.handlerSubmitClick.bind(this);
     this.handlerAfterUpload = this.handlerAfterUpload.bind(this);
+    this.showPreview = this.showPreview.bind(this);
 
     this.state = {
       init : true,
@@ -20,7 +21,11 @@ class Entity extends BaseComponent {
       },
       saving : false,
       saved : false,
-      saveTime : null
+      saveTime : null,
+      x: 0,
+      y: 0,
+      w: 0,
+      h: 0
     }
   }
 
@@ -52,24 +57,66 @@ class Entity extends BaseComponent {
     //    });
     //  }
     //}
+
   }
 
-  handlerSubmitClick(e){}
+  handlerSubmitClick(e){
+    console.log(this.state);
+
+    this.setState({
+      saving: true
+    });
+
+    let st = this.state;
+
+    $.ajax ({
+      url: SITE_URL + "services/service.php",
+      type: "GET",
+      data: {m: 'home', a: 'save_avatar', ajax: 1,user_avartar: st.uploadReault.src ,x: st.x ,y:st.y ,h:st.h ,w:st.w  },
+      cache: false,
+      async: false,
+      dataType: "json",
+      success: function (result) {
+        console.log(result);
+        if (parseInt(result.status) == 1) {
+          util.msgBox('修改成功','icon-glad');
+          location.reload();
+        } else {
+          util.msgBox(result.msg,'icon-sad');
+        }
+        this.setState({
+          saving: false
+        });
+      }.bind(this),
+      error: function (xhr, status, err) {
+        util.showError('save_avatar', status, err);
+        this.setState({
+          saving: false
+        });
+      }.bind(this)
+    });
+  }
 
   handlerAfterUpload(result){
     if (result && result != null) {
       let url = [SITE_PATH, result.src].join('');
-      console.log(url);
       $('#cropimg').attr('src',url);
       $("#cropimg").Jcrop({
+        allowMove: false,
         trackDocument: false,
         keySupport: false,
+        drawBorders: true,
+        fixedSupport: true,
+        createHandles: ['nw','ne','se','sw'],
+        handleOpacity: 0.8,
+        handleSize: 9,
+        dragEdges: true,
         onChange: this.showPreview,
         onSelect: this.showPreview,
         aspectRatio: 1,
-        setSelect: [result.mark.x, result.mark.y, result.mark.w, result.mark.h],
-        setImage: url
+        setSelect: [result.mark.x, result.mark.y, result.mark.w, result.mark.h]
       });
+      $('.jcrop-holder').find('img').attr('src',url)
     }
 
     this.setState({
@@ -79,6 +126,7 @@ class Entity extends BaseComponent {
   }
 
   showPreview(coords) {
+    let x,y,w,h;
     if (parseInt(coords.w) > 0)
     {
       var rx = 150 / coords.w;
@@ -108,11 +156,43 @@ class Entity extends BaseComponent {
         marginLeft: '-' + Math.round(rx3 * coords.x) + 'px',
         marginTop: '-' + Math.round(ry3 * coords.y) + 'px'
       });
+
+      x = coords.x;
+      y = coords.y;
+      w = coords.w;
+      h = coords.h;
+    } else {
+      jQuery('#big_img').css({
+        width: '150px',
+        height: '150px',
+        margin: 0
+      });
+
+      jQuery('#middle_img').css({
+        width: '50px',
+        height: '50px',
+        margin: 0
+      });
+
+      jQuery('#small_img').css({
+        width: '30px',
+        height: '30px',
+        margin: 0
+      });
+
+      x = 0;
+      y = 0;
+      w = 270;
+      h = 270;
+
     }
-    $('#x').val(coords.x);
-    $('#y').val(coords.y);
-    $('#w').val(coords.w);
-    $('#h').val(coords.h);
+
+    this.setState({
+      x: x,
+      y: y,
+      w: w,
+      h: h
+    });
   }
 
   render() {
@@ -136,21 +216,21 @@ class Entity extends BaseComponent {
               <span className="preview_desc">头像预览，你上传的头像会自动生成3种尺寸，请注意中小尺寸的头像是否清晰。</span>
               <div className="preview_img">
                 <div className="preview_big">
-                  <img src={this.state.uploaded ? cropimg_url : ui.img_thumb} alt="" className="big_img" id="big_img"/>
+                  <img src={this.state.uploaded ? cropimg_url : ui.img_thumb} alt="" className="big_img preview_big" id="big_img"/>
                 </div>
                 <span>大尺寸头像</span>
                 <label>150 x 150</label>
               </div>
               <div className="preview_img">
                 <div className="preview_middle">
-                  <img src={this.state.uploaded ? cropimg_url : ui.img_thumb} alt="" className="middle_img" id="middle_img"/>
+                  <img src={this.state.uploaded ? cropimg_url : ui.img_thumb} alt="" className="middle_img preview_middle" id="middle_img"/>
                 </div>
                 <span>中尺寸头像</span>
                 <label>50 x 50</label>
               </div>
               <div className="preview_img">
                 <div className="preview_small">
-                  <img src={this.state.uploaded ? cropimg_url : ui.img_thumb} alt="" className="small_img" id="small_img"/>
+                  <img src={this.state.uploaded ? cropimg_url : ui.img_thumb} alt="" className="small_img preview_small" id="small_img"/>
                 </div>
                 <span>小尺寸头像</span>
                 <label>30 x 30</label>
